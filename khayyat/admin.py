08b0a885e1210c4db04e17ed622eb33012cd70d5
Erfan Tavana <84908961+ErfanTavana@users.noreply.github.com>
2024-11-21 05:13:58 -0800

    Update admin.py

--- khayyat/admin.py
+++ khayyat/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Order
 
-
-
-admin.site.register(Order)
-
-
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('fullname', 'phone', 'fabric', 'pants', 'shirt', 'fabric_length', 'date', 'deposit', 'wage', 'delivered')
+    list_filter = ('delivered',)
+    search_fields = ('fullname', 'phone', 'fabric', 'pants', 'shirt', 'date')
+    ordering = ('-date',)
